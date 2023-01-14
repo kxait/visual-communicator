5534630a2de542a947abf9ffbe59869a51343f28
kacperdobrystudent <kacper.kuriata@student.pk.edu.pl>
2023-01-14 22:17:59 +0100

    text correction

--- frontend/Settings.js
+++ frontend/Settings.js
@@ -13,7 +13,7 @@ const Settings = () => {
         $$("span", { style: "font-family: monospace", innerText: userId }));
 
     const usernameElem = regeneratable(({ username = "" }) =>
-        $$("input", { type: "text", maxLength: "30", value: username, placeholder: "nazwa użytkownika" }));
+        $$("input", { type: "text", maxLength: "30", value: username, placeholder: "Nazwa użytkownika" }));
 
     const redo = () => sendSocket(createWhoAmIMessage())
         .then(whoAmI => {
@@ -33,7 +33,7 @@ const Settings = () => {
 
         sendSocket(createClientRenameMe(username))
             .then(data => {
-                createNotification({ text: "udalo sie zmienic nazwe" });
+                createNotification({ text: "Udało się zmienić nazwę." });
                 redo();
             });
     }
@@ -57,11 +57,11 @@ const Settings = () => {
 
         })().then(({ success, data }) => {
             if(!success) {
-                createError("hasla musza byc takie same")
+                createError("Hasła muszą być takie same.")
             } else {
                 sendSocket(createClientChangeMyPassword(data.sha256SaltedOldPassword, data.salt, data.sha256NewPassword))
                     .then(data => {
-                        createNotification({ text: "udalo sie zmienic haslo" });
+                        createNotification({ text: "Udało się zmienić hasło." });
                         redo();
                     });
             }
@@ -74,7 +74,7 @@ const Settings = () => {
 
         sendSocket(createAdminCreateNewUser(username, password, false))
             .then(data => {
-                createNotification({ text: "udalo sie stworzyc uzytkownika" });
+                createNotification({ text: "Udało się stworzyć użytkownika" });
                 redo();
             })
     }
@@ -82,38 +82,38 @@ const Settings = () => {
     const onChangeUserActivated = (userId, activated) => {
         sendSocket(createAdminChangeUserActivated(userId, activated))
             .then(_ => {
-                createNotification({ text: "udalo sie " + (activated ? "aktywowac" : "deaktywowac") + " uzytkownika" })
+                createNotification({ text: "Udało się " + (activated ? "aktywować" : "deaktywować") + " użytkownika." })
                 redo();
             });
     }
 
     const onUserChangePassword = userId => {
-        const newPassword = prompt("nowe haslo?");
+        const newPassword = prompt("Nowe hasło?");
         sha256(newPassword).then(newPasswordHashed => 
             sendSocket(createAdminChangeUserPassword(userId, newPasswordHashed))
                 .then(_ => {
-                    createNotification({ text: "udalo sie zmienic haslo uzytkownika" })
+                    createNotification({ text: "Udało się zmienić hasło użytkownika." })
                     redo();
                 }));
     }
 
     const onChangeUserName = userId => {
-        const newName = prompt("nowa nazwa?");
+        const newName = prompt("Nowa nazwa?");
         sendSocket(createAdminRenameUser(userId, newName))
             .then(_ => {
-                createNotification({ text: "udalo sie zmienic nazwe uzytkownika" })
+                createNotification({ text: "Udało się zmienić nazwę użytkownika." })
                 redo();
             });
     }
 
     const allUsers = regeneratable(({ users = [] }) => {
         const getUserIcon = isAdmin => isAdmin
-            ? Icon({ path: "icons/shield.png", title: "admin" })
-            : Icon({ path: "icons/user.png", title: "zwykly uzytkownik" });
+            ? Icon({ path: "icons/shield.png", title: "Admin" })
+            : Icon({ path: "icons/user.png", title: "Zwykły użytkownik" });
 
         const getActivateDeactivateIconAction = (userId, activated) => ClickableIcon({ 
                 path: activated ? "icons/cog_delete.png" : "icons/cog_add.png",
-                title: activated ? "dezaktywuj" : "aktywuj",
+                title: activated ? "Dezaktywuj" : "Aktywuj",
                 onclick: () => onChangeUserActivated(userId, !activated)
             });
 
@@ -121,12 +121,12 @@ const Settings = () => {
             users.map(user => $$("div", {}, [
                 ClickableIcon({ 
                     path: "icons/key.png", 
-                    title: "zmien haslo" ,
+                    title: "Zmień hasło" ,
                     onclick: () => onUserChangePassword(user.id)
                 }),
                 ClickableIcon({ 
                     path: "icons/user_edit.png", 
-                    title: "zmien nazwe",
+                    title: "Zmień nazwę",
                     onclick: () => onChangeUserName(user.id)
                 }),
                 getActivateDeactivateIconAction(user.id, user.activated),
@@ -141,27 +141,27 @@ const Settings = () => {
             ? $$("div", { }, [
                 $$("div", {}, [
                     Icon({ path: "icons/accept.png" }),
-                    $$("span", { innerText: "jestes administratorem!"})
+                    $$("span", { innerText: "Jesteś administratorem!"})
                 ]),
                 CoolButton({ style: "width: fit-content", onclick: () => {
                     setMainPanelState({ mainPanelState: MainPanelState.logs });
                 }, text: "logi", src: "icons/application_view_list.png" }),
                 $$("fieldset", {}, [
-                    $$("legend", { innerText: "ustawienia administratora" }),
+                    $$("legend", { innerText: "Ustawienia administratora" }),
                     $$("div", {}, [
-                        $$("div", { innerText: "stwórz użytkownika", style: "font-weight: bold" }),
+                        $$("div", { innerText: "Stwórz użytkownika", style: "font-weight: bold" }),
                         $$("div", { className: "text-box" }, [
-                            $$("input", { type: "text", id: "new-user-username", placeholder: "nazwa" }),
-                            $$("input", { type: "text", id: "new-user-password", placeholder: "hasło" }),
+                            $$("input", { type: "text", id: "new-user-username", placeholder: "Nazwa" }),
+                            $$("input", { type: "text", id: "new-user-password", placeholder: "Hasło" }),
                         ]),
                         $$("button", { className: "cool button", onclick: onAddUser }, [
                             Icon({ path: "icons/user_add.png" }),
-                            $$("span", { innerText: "stwórz" })
+                            $$("span", { innerText: "Stwórz" })
                         ])
                     ])
                 ]),
                 $$("fieldset", {}, [
-                    $$("legend", { innerText: "wszyscy użytkownicy"}),
+                    $$("legend", { innerText: "Wszyscy użytkownicy"}),
                     allUsers.elem
                 ])                
             ])
@@ -170,35 +170,35 @@ const Settings = () => {
 
     return $$("div", {}, [
         $$("fieldset", {}, [
-            $$("legend", { innerText: "dane o użytkowniku" }),
+            $$("legend", { innerText: "Dane o użytkowniku" }),
             $$("div", {}, [
-                $$("span", { innerText: "id użytkownika:", style: "font-weight: bold; margin-right: 5px"}),
+                $$("span", { innerText: "ID użytkownika:", style: "font-weight: bold; margin-right: 5px"}),
                 userIdElem.elem
             ])
         ]),
         $$("fieldset", {}, [
-            $$("legend", { innerText: "ustawienia użytkownika"}),
+            $$("legend", { innerText: "Ustawienia użytkownika"}),
             $$("div", {}, [
-                $$("div", { innerText: "nazwa użytkownika", style: "font-weight: bold" }),
+                $$("div", { innerText: "Nazwa użytkownika", style: "font-weight: bold" }),
                 $$("div", { className: "text-box" }, [
                     usernameElem.elem
                 ]),
                 $$("button", { className: "cool button", onclick: onChangeUsername }, [
                     Icon({ path: "icons/textfield_rename.png" }),
-                    $$("span", { innerText: "zmień nazwę" })
+                    $$("span", { innerText: "Zmień nazwę" })
                 ])
             ]),
             $$("div", { innerHTML: "&nbsp;" }),
             $$("div", {}, [
-                $$("div", { innerText: "hasło", style: "font-weight: bold" }),
+                $$("div", { innerText: "Hasło", style: "font-weight: bold" }),
                 $$("div", { className: "text-box" }, [
-                    $$("input", { type: "password", id: "current-password", placeholder: "aktualne hasło" }),
-                    $$("input", { type: "password", id: "new-password", placeholder: "nowe hasło" }),
-                    $$("input", { type: "password", id: "new-password-confirm", placeholder: "potwierdź hasło" })
+                    $$("input", { type: "password", id: "current-password", placeholder: "Aktualne hasło" }),
+                    $$("input", { type: "password", id: "new-password", placeholder: "Nowe hasło" }),
+                    $$("input", { type: "password", id: "new-password-confirm", placeholder: "Potwierdź hasło" })
                 ]),
                 $$("button", { className: "cool button", onclick: onChangePassword }, [
                     Icon({ path: "icons/key_go.png" }),
-                    $$("span", { innerText: "zmień hasło" })
+                    $$("span", { innerText: "Zmień hasło" })
                 ])
             ])
         ]),
